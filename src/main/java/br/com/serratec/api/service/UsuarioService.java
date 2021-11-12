45a9270cc6b6c8c96206450e551b9876645d6d0c
CasttCK <vini.kronemberger@gmail.com>
2021-11-12 20:46:21 -0300

    Correção de inicialização

--- src/main/java/br/com/serratec/api/service/UsuarioService.java
+++ src/main/java/br/com/serratec/api/service/UsuarioService.java
@@ -2,8 +2,11 @@ package br.com.serratec.api.service;
 
 import java.util.Optional;
 
+import org.springframework.stereotype.Component;
+
 import br.com.serratec.api.model.UsuarioLogin;
 
+@Component
 public class UsuarioService {
 
 	public Optional<UsuarioLogin> Logar(Optional<UsuarioLogin> user) {
